60fea107c7772b6c955adfd3dfc00b3a352352a5
Matheus Spindula <matheusluiz98@hotmail.com>
2023-07-24 20:57:27 -0300

    Componentes de Box,Text e Heading

--- aulas/05-design-system/packages/react/src/index.tsx
+++ aulas/05-design-system/packages/react/src/index.tsx
@@ -1,31 +1,3 @@
-import { ComponentProps } from 'react'
-
-import { styled } from './styles'
-
-export const Button = styled('button', {
-  fontFamily: '$default',
-  backgroundColor: '$ignite300',
-  borderRadius: '$md',
-  border: '$small',
-  fontWeight: 'bold',
-  color: '$white',
-
-  defaultVariants: {
-    size: 'small',
-  },
-
-  variants: {
-    size: {
-      small: {
-        fontSize: 14,
-        padding: '$2 $4',
-      },
-      big: {
-        fontSize: 16,
-        padding: '$3 $6',
-      },
-    },
-  },
-})
-
-export type ButtonProps = ComponentProps<typeof Button>
+export * from './components/Box'
+export * from './components/Text'
+export * from './components/Heading'
