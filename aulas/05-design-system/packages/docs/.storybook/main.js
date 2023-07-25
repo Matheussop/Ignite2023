d7e487b5539541db8795b1a98dc8eca00a4840fc
Matheus Spindula <matheusluiz98@hotmail.com>
2023-07-25 18:10:41 -0300

    Fixed path deploy and subpath to Storybook on production

--- aulas/05-design-system/packages/docs/.storybook/main.js
+++ aulas/05-design-system/packages/docs/.storybook/main.js
@@ -18,5 +18,12 @@ const config = {
   docs: {
     autodocs: "tag",
   },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/Ignite2023/'
+    }
+
+    return config
+  }
 };
 export default config;
