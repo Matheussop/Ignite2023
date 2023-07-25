849e46ea229932c7548d0c098cac7874d9a01d0e
Matheus Spindula <matheusluiz98@hotmail.com>
2023-07-24 22:06:55 -0300

    Refactor : Melhorando stories e novo addon de acessibilidade

--- aulas/05-design-system/packages/docs/src/stories/Avatar.stories.tsx
+++ aulas/05-design-system/packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/matheussop.png',
     alt: 'Matheus Spindula',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
--- aulas/05-design-system/packages/docs/src/stories/Text.stories.tsx
+++ aulas/05-design-system/packages/docs/src/stories/Text.stories.tsx
@@ -8,6 +8,28 @@ export default {
     children:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus perspiciatis eius vel, magnam sequi accusamus?',
   },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio',
+      },
+    },
+  },
 } as Meta<TextProps>
 
 export const Primary: StoryObj<TextProps> = {}
